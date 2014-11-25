a59d0fb3596c21fd9cf228c7a512f650589544ec
Jedrzej Nowak <jedrzej.nowak@codernity.com>
2014-11-25 20:07:09 +0100

    Tests improved

--- test/exelli_test.exs
+++ test/exelli_test.exs
@@ -1,12 +1,34 @@
 defmodule ExelliTest.Test.Handler do
   use ExUnit.Case
 
-  setup do
-    {:ok, pid} = Exelli.elli_start Exelli.TestHandler
+  @base_url "http://localhost:4000/"
+
+  defmacro get!(target) do
+    quote do
+      HTTPoison.get! (unquote(@base_url) <> unquote(target))
+    end
+  end
+
+  setup_all do
+    {:ok, pid} = Exelli.elli_start Exelli.TestHandler.Simple
     {:ok, pid: pid}
   end
 
-  test "ping" do
-    assert 1 + 1 == 2
+  test "very basic one" do
+    res = get! ""
+    assert res.status_code == 200
+    assert res.body == "HELLO"
+  end
+
+  test "very basic one #2" do
+    res = get! "/ping"
+    assert res.status_code == 200
+    assert res.body == "PONG"
+  end
+
+  test "very basic one #3" do
+    res = get! "/test"
+    assert res.status_code == 200
+    assert res.body == "WORKS"
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,24 +5,20 @@ Application.ensure_all_started(:httpoison)
 Logger.configure_backend(:console, colors: [enabled: false], metadata: [:request_id])
 
 
-defmodule Exelli.TestHandler do
+defmodule Exelli.TestHandler.Simple do
   use Exelli.Handler
 
-  def handle(:GET, ["test", a], _req, _args) when a == "a" do
-    {:ok, "A"}
+  get [] do
+    {:ok, "HELLO"}
   end
 
   get ["ping"] do
     {:ok, "PONG"}
 
   end
-  
+
   get ["test"] do
     {:ok, "WORKS"}
-  end
-
-  get ["test", y, yy] when y == "1" and yy == "2" do
-    {:ok, "y"}
   end
 
 end
